5f2f42a57fa32f531706c9fb0c00bd30e4302f21
Zack Cerza <zack@redhat.com>
2021-12-15 16:51:42 -0700

    f

--- .functional_ci_setup.py
+++ .functional_ci_setup.py
@@ -107,8 +107,8 @@ def get_ocsci_conf(pre_upgrade=False):
     if pre_upgrade:
         version = Version.coerce(
             env["OCS_REGISTRY_IMAGE"].split(":")[1]).truncate("minor")
+        conf_obj["DEPLOYMENT"] = dict(ocp_version=f"{version.major}.{version.minor}-ga")
         version.minor -= 1
-        conf_obj["DEPLOYMENT"] = dict(ocs_version=f"{version}-ga")
         conf_obj["ENV_DATA"]["ocs_version"] = f"{version.major}.{version.minor}"
     else:
         conf_obj["DEPLOYMENT"] = dict(ocs_registry_image=env["OCS_REGISTRY_IMAGE"])
