a447723a4e75f8a7b2c1c08ebaae51aebe040184
Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>
2024-11-22 11:34:07 +0530

    testing
    Signed-off-by: Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>

--- tests/conftest.py
+++ tests/conftest.py
@@ -8161,7 +8161,9 @@ def reduce_expiration_interval(add_env_vars_to_noobaa_core_class):
 
 
 @pytest.fixture()
-def change_lifecycle_batch_size(add_env_vars_to_noobaa_core_class):
+def change_lifecycle_batch_size(
+    add_env_vars_to_noobaa_core_class, add_env_vars_to_noobaa_endpoint_class
+):
     """
     Change the batch size for the object lifecycle
     by modifying the environment variable LIFECYCLE_BATCH_SIZE
@@ -8175,6 +8177,10 @@ def change_lifecycle_batch_size(add_env_vars_to_noobaa_core_class):
 
         """
         add_env_vars_to_noobaa_core_class(
+            [(constants.LIFECYCLE_BATCH_SIZE_PARAM, new_lifecycle_batch_size)]
+        )
+
+        add_env_vars_to_noobaa_endpoint_class(
             [(constants.LIFECYCLE_BATCH_SIZE_PARAM, new_lifecycle_batch_size)]
         )
 
